6c47bb53eb1833c24aaba895df46d518ade5b128
Olga Chitembo <olga.chitembo@metropolia.fi>
2026-04-17 22:57:32 +0300

    fix test

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -2,20 +2,35 @@ package org.example;
 
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.mockito.MockedStatic;
 import org.testfx.framework.junit5.ApplicationTest;
 
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class MainTest extends ApplicationTest {
 
     private Stage stage;
+    private MockedStatic<org.example.service.LocalizationService> mock;
+
+    @Override
+    public void init() throws Exception {
+        mock = mockStatic(org.example.service.LocalizationService.class);
+        mock.when(() -> org.example.service.LocalizationService.getStrings(anyString()))
+                .thenReturn(Map.of("app.name", "Olga Chitembo/Shopping Cart"));
+    }
 
     @Override
     public void start(Stage stage) throws Exception {
         this.stage = stage;
-        new Main().start(stage);
+        new Main().start(stage); // now safe ✅
+    }
+
+    @Override
+    public void stop() throws Exception {
+        mock.close(); // cleanup
     }
 
     @Test
